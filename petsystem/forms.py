cf79ed0039eb7e8d9fd999ea779235455ec6ff0b
maggymbithi <magretmbithi8@gmail.com>
2024-10-29 22:49:36 -0700

    updates

--- petsystem/forms.py
+++ petsystem/forms.py
@@ -4,4 +4,4 @@ from .models import Pets
 class PestForm(forms.ModelForm):
     class Meta:
         model = Pets
-        fields = ['name', 'location', 'image']
+        fields = ['name', 'location', 'image','age','likes','dislikes','contact']
--- petsystem/models.py
+++ petsystem/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Pets(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=150)
+    age=models.IntegerField(default=0, null=False)
+    contact=models.IntegerField(default='0706703863',null=False)
+    likes=models.CharField(max_length=20, blank=True, null=False)
+    dislikes=models.CharField(max_length=20, blank=True, null=False)
     image = models.ImageField(upload_to='pests/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
